055b3ed046b49da9222d760ea8e210393569f977
Luis De Anda <luisdeanda916@outlook.com>
2019-06-11 13:51:27 -0700

    callbacks.js finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,3 +1,4 @@
+console.log("********** START OF CALLBACKS.JS **********");
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
@@ -27,24 +28,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length) {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, function(lastItem) {
+  console.log(lastItem);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(5, 10, function(adds) {
+  console.log(adds);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(5, 10, function(multiplied) {
+  console.log(multiplied);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list.includes(item)) {
+      return cb(true);
+    } else {
+      return cb(false);
+    }
+  }
 }
+contains("yo-yo", items, function(check) {
+  console.log(check);
+})
 
 /* STRETCH PROBLEM */
 
@@ -53,3 +80,6 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+
+console.log("********** END OF CALLBACKS.JS **********");
